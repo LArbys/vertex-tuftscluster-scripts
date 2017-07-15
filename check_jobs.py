33fc0ae51f72a9473847f763c4da3e5836cbf851
Taritree Wongjirad <taritree.wongjirad@gmail.com>
2017-07-15 01:08:18 -0400

    update

---
+++ check_jobs.py
@@ -0,0 +1,68 @@
+import os,sys
+import ROOT as rt
+
+# Check job id list. Check output folder. Check that tagger output files have entries (and same number of entries)
+# based on checks, will produce rerun list
+
+# MCC8.1 nue+cosmic: Maccfrey
+#VERTEX_FOLDER="/home/taritree/larbys/data/mcc8.1/nue_1eNpfiltered/out_week0626/vertex"
+
+# MCC8.1 nue only: Tufts
+#VERTEX_FOLDER="/cluster/kappa/90-days-archive/wongjiradlab/larbys/data/mcc8.1/nue_nocosmic_1eNpfiltered/out_week0626/vertex"
+
+VERTEX_FOLDER="/cluster/kappa/90-days-archive/wongjiradlab/larbys/data/mcc8.1/nue_1eNpfiltered/out_week0626/vertex"
+
+files = os.listdir(VERTEX_FOLDER)
+
+file_dict = {}
+for f in files:
+    f.strip()
+    idnum = int(f.split("_")[-1].split(".")[0])
+    if idnum not in file_dict:
+        file_dict[idnum] = {"vertexana":None,"vertexout":None}
+    if "vertexana" in f:
+        file_dict[idnum]["vertexana"] = VERTEX_FOLDER+"/"+f
+    elif "vertexout" in f:
+        file_dict[idnum]["vertexout"] = VERTEX_FOLDER+"/"+f
+
+ids = file_dict.keys()
+ids.sort()
+
+
+rerun_list = []
+good_list = []
+for fid in ids:
+    try:
+        rfile_vertexout = rt.TFile( file_dict[fid]["vertexout"] )
+        tree = rfile_vertexout.Get("pixel2d_test_img_tree")
+        nentries_out = tree.GetEntries()
+
+        rfile_vertexana = rt.TFile( file_dict[fid]["vertexana"] )
+        tree = rfile_vertexana.Get("EventVertexTree")
+        nentries_ana = tree.GetEntries()
+
+        good_list.append(fid)
+    except:
+        print "Not ok: ",fid
+        rerun_list.append(fid)
+        continue
+
+print "Goodlist: ",len(good_list)
+print "Rerunlist: ",len(rerun_list)
+
+# read in jobidlist
+fjobid = open("jobidlist.txt",'r')
+ljobid = fjobid.readlines()
+for l in ljobid:
+    jobid = int(l.strip())
+    if jobid in good_list or jobid in rerun_list:
+        continue
+    else:
+        rerun_list.append(jobid)
+fjobid.close()
+
+
+frerun = open("rerunlist.txt",'w')
+for jobid in rerun_list:
+    print >> frerun,jobid
+frerun.close()
--- submit.sh
+++ submit.sh
@@ -3,10 +3,23 @@
 #SBATCH --job-name=vertex
 #SBATCH --output=vertex_log.txt
 #
-#SBATCH --ntasks=3
-#SBATCH --time=24:00:00
+#SBATCH --ntasks=10
+#SBATCH --time=2:00:00
 #SBATCH --mem-per-cpu=4000
 
-module load singularity
+CONTAINER=/cluster/kappa/90-days-archive/wongjiradlab/larbys/images/dllee_unified/singularity-dllee-unified-071017.img
+WORKDIR=/cluster/kappa/90-days-archive/wongjiradlab/grid_jobs/vertex-tuftscluster-scripts
+INPUTLISTDIR=${WORKDIR}/inputlists
+JOBIDLIST=${WORKDIR}/rerunlist.txt
 
-srun singularity exec /cluster/kappa/90-days-archive/wongjiradlab/larbys/images/dllee_unified/singularity-dllee-unified-062517.img bash -c "cd /cluster/home/twongj01/grid_jobs/vertex-tuftscluster-scripts && source run_job.sh /cluster/home/twongj01/grid_jobs/vertex-tuftscluster-scripts /cluster/home/twongj01/grid_jobs/vertex-tuftscluster-scripts/inputlists /cluster/kappa/90-days-archive/wongjiradlab/larbys/data/mcc8.1/nue_1eNpfiltered/out_week0626/vertex /cluster/home/twongj01/grid_jobs/vertex-tuftscluster-scripts/rerunlist.txt"
+OUTPUTDIR=/cluster/kappa/90-days-archive/wongjiradlab/larbys/data/mcc8.1/numu_1muNpfiltered/out_week071017/vertex
+#OUTPUTDIR=/cluster/kappa/90-days-archive/wongjiradlab/larbys/data/mcc8.1/nue_1eNpfiltered/out_week071017/vertex
+
+mkdir -p ${OUTPUTDIR}
+module load singularity
+srun singularity exec ${CONTAINER} bash -c "cd ${WORKDIR} && source run_job.sh ${WORKDIR} ${INPUTLISTDIR} ${OUTPUTDIR} ${JOBIDLIST}"
+
+# USE THE FOLLOWING COMMAND TO RUN A TEST INTERACTIVELY
+# (but do this from an interactive slurm job)
+# (to start such an interactive job, run: srun --pty -p interactive bash)
+#singularity exec ${CONTAINER} bash -c "export SLURM_PROCID=0 && cd ${WORKDIR} && source run_job.sh ${WORKDIR} ${INPUTLISTDIR} ${OUTPUTDIR} ${JOBIDLIST}"
